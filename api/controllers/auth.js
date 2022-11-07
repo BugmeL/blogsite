f1e612e68c2d6fec0ecfaed5d2fba3af1ccc4999
BugmeL <reymel.aicpa@gmail.com>
2022-11-07 22:10:50 +0800

    blog Api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -16,10 +16,10 @@ export const register = (req, res) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-    const values = [req.body.username, req.body.email, hash];
+    const values = [req.body.username, req.body.email, hash,];
 
     db.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
+      if (err) return res.json(err);
       return res.status(200).json("User has been created.");
     });
   });
@@ -32,28 +32,39 @@ export const login = (req, res) => {
   const q = "SELECT * FROM users WHERE username = ?";
 
   db.query(q, [req.body.username], (err, data) => {
-    if (err) return res.status(500).json(err);
+    if (err) return res.json(err);
     if (data.length === 0) return res.status(404).json("User not found!");
 
+      console.log(req.body.password)
+      console.log(data)
+
     //Check password
-    const isPasswordCorrect = bcrypt.compareSync(
-      req.body.password,
-      data[0].password
-    );
+    const isPasswordCorrect = bcrypt.compareSync
+      (req.body.password, data[0].password);
+    
+      if (!isPasswordCorrect)
+         return res.status(200).json("Login");
+      
 
-    if (!isPasswordCorrect)
-      // return res.status(400).json("Wrong username or password!");
-      return res.status(200).json("Login Success!");
+console.log(isPasswordCorrect)
+      
+     const token = jwt.sign({ id: data[0].id }, "jwtkey");
+     const{password, ...other} = data[0];
+    
+   res
+   .cookie("access_token", token, {
+    httpOnly:true
+   })
 
-    // const token = jwt.sign({ id: data[0].id }, "jwtkey");
-    // const { password, ...other } = data[0];
 
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json(other);
+     console.log(token)
+      
+      return res.status(200).json({
+        c: 200,
+        m: "Login Success",
+        d: data[0]
+      });
+
   });
 };
 
